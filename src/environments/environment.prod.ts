6639dfb4b4ee8f90ead415fa86b5b92bfb0eab55
Mick Evans <bitshifted.development@gmail.com>
2018-04-01 19:52:55 -0400

    chore: Added Firebase config to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB92XypwT-OG92n4IN2pmo4FMbYYbVQE_w",
+    authDomain: "bitshifted-spark.firebaseapp.com",
+    databaseURL: "https://bitshifted-spark.firebaseio.com",
+    projectId: "bitshifted-spark",
+    storageBucket: "bitshifted-spark.appspot.com",
+    messagingSenderId: "50041902546"
+  }
 };
